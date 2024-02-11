82250484ba837f30ea4f5fb699a753c0e44d5126
Samiksha Kad <naveenpatidar3331@gmail.com>
2024-02-11 18:22:04 +0530

    view request

--- windeep_app/src/components/member/ViewRequests.jsx
+++ windeep_app/src/components/member/ViewRequests.jsx
@@ -14,75 +14,306 @@ export default function ViewRequests() {
     }
 }
 
-    const [viewRequests, setViewRequests] = useState([]);
+const [viewRequests, setViewRequests] = useState({Pending:[],Reject:[],Complete:[]});
 
-  useEffect(() => {
-    // setViewRequests(memberDetails);
-    fetch('http://localhost:9000/member/requestLoan', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          id:id      
-        })
-      })
-        .then(response => response.json())
-        .then(result => {
-          console.log('API Response: ', result.payload[0].result);
-          setViewRequests(result.payload[0].result);
-        })
-        .catch(error => {
-          console.error('API Error:', error);
-        });
-  }, []);
+const [page, setPage] = useState({Pending:0,Reject:0,Complete:0});
+const [rowsPerPage, setRowsPerPage] = useState({Pending:10,Reject:10,Complete:10});
 
+useEffect(() => {
+  // setViewRequests(memberDetails);
+  // fetch('http://localhost:9000/member/requestLoan', {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify({
+  //       id:id      
+  //     })
+  //   })
+  //     .then(response => response.json())
+  //     .then(result => {
+  //       console.log('API Response: ', result.payload[0].result);
+  //       setViewRequests(result.payload[0].result);
+  //     })
+  //     .catch(error => {
+  //       console.error('API Error:', error);
+  //     });
+  const result = [
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": null,
+        "amount": "100000",
+        "period": 2,
+        "unit": "months",
+        "emi": "50313",
+        "processing_fee": "1500"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": null,
+        "amount": "100000",
+        "period": 2,
+        "unit": "months",
+        "emi": "50313",
+        "processing_fee": "1500"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": "Pradeep Khengare",
+        "amount": "100000",
+        "period": 2,
+        "unit": "months",
+        "emi": "50313",
+        "processing_fee": "1500"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": "Samiksha Kad",
+        "amount": "100000",
+        "period": 2,
+        "unit": "months",
+        "emi": "50313",
+        "processing_fee": "1500"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": null,
+        "amount": "100000",
+        "period": 2,
+        "unit": "years",
+        "emi": "",
+        "processing_fee": "1.5"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": "Pradeep Khengare",
+        "amount": "100000",
+        "period": 2,
+        "unit": "years",
+        "emi": "",
+        "processing_fee": "1.5"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": "Samiksha Kad",
+        "amount": "100000",
+        "period": 2,
+        "unit": "years",
+        "emi": "",
+        "processing_fee": "1.5"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": null,
+        "amount": "100000",
+        "period": 2,
+        "unit": "years",
+        "emi": "4387",
+        "processing_fee": "1500"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": "Pradeep Khengare",
+        "amount": "100000",
+        "period": 2,
+        "unit": "years",
+        "emi": "4387",
+        "processing_fee": "1500"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": "Samiksha Kad",
+        "amount": "100000",
+        "period": 2,
+        "unit": "years",
+        "emi": "4387",
+        "processing_fee": "1500"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": null,
+        "amount": "100000",
+        "period": 2,
+        "unit": "months",
+        "emi": "50313",
+        "processing_fee": "1500"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": "Pradeep Khengare",
+        "amount": "100000",
+        "period": 2,
+        "unit": "months",
+        "emi": "50313",
+        "processing_fee": "1500"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": "Samiksha Kad",
+        "amount": "100000",
+        "period": 2,
+        "unit": "months",
+        "emi": "50313",
+        "processing_fee": "1500"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": null,
+        "amount": "100000",
+        "period": 5,
+        "unit": "months",
+        "emi": "20251",
+        "processing_fee": "1500"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": "Pradeep Khengare",
+        "amount": "100000",
+        "period": 5,
+        "unit": "months",
+        "emi": "20251",
+        "processing_fee": "1500"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": "Samiksha Kad",
+        "amount": "100000",
+        "period": 5,
+        "unit": "months",
+        "emi": "20251",
+        "processing_fee": "1500"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": null,
+        "amount": "100000",
+        "period": 2,
+        "unit": "months",
+        "emi": "50313",
+        "processing_fee": "1500"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": "Pradeep Khengare",
+        "amount": "100000",
+        "period": 2,
+        "unit": "months",
+        "emi": "50313",
+        "processing_fee": "1500"
+    },
+    {
+        "status": "Pending",
+        "admin_reason": null,
+        "admin_name": "Samiksha Kad",
+        "amount": "100000",
+        "period": 2,
+        "unit": "months",
+        "emi": "50313",
+        "processing_fee": "1500"
+    }
+].reduce((acc,curr)=>{
+    if(curr.status=="Reject"){
+        acc.Reject.push(curr)
+    }
+    if(curr.status=="Complete"){
+        acc.Complete.push(curr)
+    }
+     if(curr.status=="Pending"){
+        acc.Pending.push(curr)
+    }
+    return acc
+},{Pending:[],Reject:[],Complete:[]})
+  setViewRequests(result)
+}, []);
 
+const columns = [
+  { id: "amount", label: "Amount: ₹", minWidth: 170 },
+  {
+    id: "Period",
+    label: "Period",
+    minWidth: 100,
+  },
+  { id: "Unit", label: "Unit", minWidth: 100 },
+  { id: "EMI", label: "EMI", minWidth: 100 },
+  { id: "ProcessingFee", label: "Processing Fee", minWidth: 100 },
+  { id: "AdminRequests", label: "Admin Requests", minWidth: 100 },
+  { id: "Reason", label: "Reason", minWidth: 100 },
+  { id: "ApprovedBy", label: "Approved By", minWidth: 100 },
+  { id: "RaisedBy", label: "Raised By", minWidth: 100 },
+];
+
+const handleChangePage = (event, newPage, key) => {
+  setPage({...page,[key]:newPage});
+};
+
+const handleChangeRowsPerPage = (event) => {
+  setRowsPerPage({...rowsPerPage,[key]:parseInt(event.target.value, 10)});
+  setPage({...page,[key]:0});
+};
 
     return (
         <>
             {console.log("viewRequests",viewRequests)}
-            {/* <table class="table table-bordered">
-          <thead>
-            <tr>
-              {columns.map((ele) => (
-                <th key={ele.label}>{ele.label} </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {viewRequests.length ? (
-              viewRequests.map((fle) => (
-                <>
-                  <tr key={fle.date} style={{ cursor: "pointer" }}>
-                    <td>{fle.start_date}</td>
-                    <td>{fle.monthly_contribution}</td>
-                    <td>{fle.total_contribution}</td>
-                    <td>{fle.bonus}</td>
-                    <td>{fle.remarks}</td>
-                    <td>{fle.updated_by}</td>
-                    <td>{fle.admin_remarks}</td>
-                    <td>{generateActionButtons(fle)}</td>
+            <table class="table table-bordered">
+              <thead>
+                <tr>
+                  {columns.map((ele) => (
+                    <th key={ele.label}>{ele.label} </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {viewRequests.Pending.length ? (
+                  viewRequests.Pending.map((fle) => (
+                    <>
+                      <tr key={fle.amount} style={{ cursor: "pointer" }}>
+                        <td>{fle.period}</td>
+                        <td>{fle.unit}</td>
+                        <td>{fle.emi}</td>
+                        <td>{fle.processing_fee}</td>
+                        <td>{fle.status}</td>
+                        <td>{fle.admin_reason}</td>
+                        <td>{fle.admin_name}</td>
+                        <td>{fle.admin_name}</td>
+                      </tr>
+                    </>
+                  ))
+                ) : (
+                  <tr>
+                    <td colSpan={columns.length}>No data available</td>
                   </tr>
-                </>
-              ))
-            ) : (
-              <tr>
-                <td colSpan={columns.length}>No data available</td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-        {data.length &&         <TablePagination
-      component="div"
-      count={totalCount}
-      page={page}
-      onPageChange={handleChangePage}
-      rowsPerPage={rowsPerPage}
-      rowsPerPageOptions={[10, 20, 30, 40, 50]}
-      onRowsPerPageChange={handleChangeRowsPerPage}
-    />} */}
-            {viewRequests.map((ele, index) => (
+                )}
+              </tbody>
+            </table>
+            {viewRequests.Pending.length &&         
+              <TablePagination
+                component="div"
+                count={viewRequests.Pending.length}
+                page={page}
+                onPageChange={(e,v)=>handleChangePage(e,v,"Pending")}
+                rowsPerPage={rowsPerPage}
+                rowsPerPageOptions={[10, 20, 30, 40, 50]}
+                onRowsPerPageChange={(e,v)=>handleChangeRowsPerPage(e,v,"Pending")}
+          />}
+            {/* {viewRequests.map((ele, index) => (
               <div className={styles.viewRequests}>
                 <Grid container spacing={1}>
                   
@@ -102,8 +333,7 @@ export default function ViewRequests() {
              
                 </Grid>
               </div>
-                   ))}
-              <br />
+                   ))} */}
         </>
     )
 } 
